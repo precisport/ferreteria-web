5a3aab52d50f98e817987c0926de89603e945e10
Francisco Mesa <franciscojaviemesaarellano@gmail.com>
2026-01-19 21:43:25 -0300

    Conexión MySQL usando variables de entorno Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ferreteria'
+  host: process.env.MYSQLHOST,
+  user: process.env.USUARIO_MYSQL,
+  password: process.env.CONTRASEÑA_MYSQL,
+  database: process.env.BASE_DE_DATOS_MYSQL,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
