1536e7305d0c6fd8a3f5141a27ace9e6478bdac3
Francisco Mesa <franciscojaviemesaarellano@gmail.com>
2026-01-24 06:42:50 -0300

    Add comment to force redeploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+// Force redeploy v5
 const multer = require("multer");
 const path = require("path");
 const db = require("./config/db");
